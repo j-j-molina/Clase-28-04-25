00c7d1db93c9b4c8a359a1626a4142597dfd6920
j-j-molina <juanj.molina@upb.edu.co>
2025-04-28 17:17:29 -0500

    added new function (random quote) on route /route

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 import express from 'express';
 const app = express();
+app.listen(3000, () => {
+    console.log('Server is runninf on port 3000');
+});
+app.get('/', (req, res) => {
+    res.send('Hello World NODEMON!');
+    }
+);
+app.get(
+    "/route",
+    (req, res) => {
+        const frases = [
+            "🚀 ¡Hoy es un gran día para desplegar un bug!",
+            "🐍 Si no funciona... ¡reinicia el router!",
+            "🍕 Código sin pizza es como servidor sin puerto.",
+            "🔥 ¡Compila sin miedo y depura después!",
+            "🧠 ¿Error 404? La motivación no encontrada.",
+            "😎 keep calm and npm install.",
+            "👾 El primer paso para arreglarlo es admitir que lo rompiste.",
+        ];
+        
+        const fraseAleatoria = frases[Math.floor(Math.random() * frases.length)];
+        res.send(fraseAleatoria);
+    });
